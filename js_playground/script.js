e6aa34feb213a384457597ea60892d89b8f9abd9
Asvin L Vinod <asvinlvinod@gmail.com>
2022-08-24 09:48:08 +0530

    commit

---
+++ js_playground/script.js
@@ -0,0 +1,13 @@
+console.log('hello world!');
+let a = 'hi';
+let b = 'world';
+let c = 10;
+console.log(a + ' ' + b);
+if (c % 2 == 0) {
+  console.log(c + ' is an even number');
+} else {
+  console.log('not an even number');
+}
+for (let i = 0; i < c; i++) {
+  console.log(i);
+}

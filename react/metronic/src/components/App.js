a02574b92576c33bf46a9df6b947bf40e1f0f4d8
Asvin L Vinod <asvinlvinod@gmail.com>
2022-09-28 18:06:02 +0530

    commit

---
+++ react/metronic/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+import Header from './Header';
+import '../app.css';
+const App = () => {
+  return (
+    // <div className="main_body">
+    //   <img
+    //     src={require('../images/background.jpeg')}
+    //     className="bg-image shadow-1-strong"
+    //   />
+
+    (<Card />), (<Header />)
+    //   <div className="header">
+    //     <img src={require('../images/logo.png')} />
+    //     <input className="search" placeholder="search" />
+    //   </div>
+    //   <i className="bi bi-chat-right-text"></i>
+    //   <i className="bi bi-grid-fill"></i>
+    // </div>
+  );
+};
+export default App;
